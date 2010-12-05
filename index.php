Holek/cite-gen

commit d1b87094ecda06f4b4202a150cf9a59255c1a39b

okay, let's do translations properly this time, all i18n-related fixes

index.php
@@ -28,9 +28,8 @@ require_once('./includes/abstractClasses.php');
 
 // I18N files
 require_once('./messages/Names.php');
-try {
-	include_once('./messages/allMessages.php');
-} catch( $e ) {
+if (!include_once('./messages/allMessages.php'))
+{
 	echo '<!doctype html>
 
 <h1>Citation generator fatal error</h1>
@@ -53,7 +52,7 @@ parse_str($_SERVER['QUERY_STRING'],$query);
 // Language
 $scriptLanguage = get('scriptlang', (($_COOKIE['languages']['script'] && preg_match( '/^[a-z-]+$/', $_COOKIE['languages']['script']))?$_COOKIE['languages']['script']:substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,2)));
 
-if (!$messages[$scriptLanguage]['Title'])
+if (getMessage('Title') != 'Title')
 {
 	$scriptLanguage='en';
 }
@@ -266,7 +265,9 @@ while(!is_bool($file = readdir($dh)))
 		{
 			if (!$isTemplate)
 			{
-				$isTemplate = true;
+				$isTemplate = true;if (!include_once('./messages/allMessages.php'))
+{
+
 			}
 			if ($ini['category'] == 'skins' && !$ini['dontstore'])
 			{
@@ -306,7 +307,7 @@ $savant->errors = $error->output_errors();
 
 $savant->input = $inputArray;
 
-$savant->lang = $messages[$scriptLanguage];
+$savant->lang = prepareLanguageArray();
 $savant->languages = $wgLanguageNames;
 $savant->scriptLanguage = $scriptLanguage;
 $savant->templateLanguage = $templateLanguage;

includes/Functions.php
@@ -251,6 +251,22 @@ function getMessage($id, $s1 = '',$s2 = '',$s3 = '')
 }
 
 /**
+ * Prepares language array to use in templates
+ * @return array
+ */
+function prepareLanguageArray()
+{
+	global $messages, $scriptLanguage;
+	$langAry = array();
+	foreach($messages[$scriptLanguage] as $key => $message) {
+		// strip "ts-citegen-" prefix from translation keys for templates
+		// length of ts-citegen- equals 11
+		$langAry[substr($key, 11)] = $message;
+	}
+	return $langAry;
+}
+
+/**
  * Function for writing debug info from within various classes.
  * @param string $message
  */

commit 97219a82ac28a17e2d45d0f29795cc6d577e6994

nope, that was a bug of wrong copy-paste introduced in d1b8709

index.php
@@ -265,9 +265,7 @@ while(!is_bool($file = readdir($dh)))
 		{
 			if (!$isTemplate)
 			{
-				$isTemplate = true;if (!include_once('./messages/allMessages.php'))
-{
-
+				$isTemplate = true;
 			}
 			if ($ini['category'] == 'skins' && !$ini['dontstore'])
 			{