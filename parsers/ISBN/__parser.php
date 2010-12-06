a0b3e7e4f3725ed3577f04cce22617e5cec04469
Mike Połtyn <mike@poltyn.com>
2010-12-06 21:18:36 +0100

    bugfix that made every ISBN parser a default one

--- parsers/ISBN/__parser.php
+++ parsers/ISBN/__parser.php
@@ -1,15 +1,12 @@
 <?php
 
-
-class ISBN_parser extends InputCheck
-{
-	static public function checkInput($input)
-	{
+class ISBN_parser extends InputCheck {
+	static public function checkInput($input) {
 		$input = ISBN_parser::prepareInput($input);
-		if (
+		return (
 		// ISBN-10
 		    ((strlen($input) == 10) &&
-		    ((($input[0]     // (1 * x1
+		    ((($input[0]    //  (1 * x1
 		     +(2*$input[1]) // + 2 * x2
 		     +(3*$input[2]) // + 3 * x3
 		     +(4*$input[3]) // + ...
@@ -23,7 +20,7 @@ class ISBN_parser extends InputCheck
 			||
 		// ISBN-13
 			((strlen($input) == 13) &&
-				(((10-                 // (10 -
+				(((10-           // (10 -
 		    (($input[0]      //  (1 * x1
 		     +(3*$input[1])  // + 3 * x2
 		     +(  $input[2])  // + 1 * x3
@@ -36,45 +33,30 @@ class ISBN_parser extends InputCheck
 		     +(3*$input[9])  // + 3 * x10
 		     +(  $input[10]) // + 1 * x11
 		     +(3*$input[11]) // + 3 * x12)
-			 )%10))%10                    // mod 10) == x13 // 0 replaces 10
+			 )%10))%10               // mod 10) == x13 // 0 replaces 10
 			 == $input[12])))
-		)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		);
 	}
 	
-	static public function listParsers()
-	{
+	static public function listParsers() {
 		global $templateLanguage,$scriptLanguage;
 		$parsersClass = opendir('./parsers/ISBN');
 		$availableParsers = array();
 		$defaultParsers = array();
-		while(!is_bool($file = readdir($parsersClass)))
-		{
-			if(substr($file, -4) == '.ini')
-			{
+		while(!is_bool($file = readdir($parsersClass))) {
+			if(substr($file, -4) == '.ini') {
 				$ini = parse_ini_file('./parsers/ISBN/'.$file,true);
-				if ($ini['translations'][$scriptLanguage])
-				{
+				if ($ini['translations'][$scriptLanguage]) {
 					$parserTitle = $ini['translations'][$scriptLanguage];
-				}
-				else
-				{
+				} else {
 					$parserTitle = $ini['summary']['title'];
 				}
 				$availableParsers[getMessage($ini['summary']['main_language'])][] = array('value'=>substr($file,0,-4),'label'=>$parserTitle);
 				
-				if ($ini['summary']['main_language'] == $templateLanguage || $ini['summary']['main_language'] = 'International')
-				{
-					$defaultParsers = array_merge(array(substr($file,0,-4)),$defaultParsers);
+				if ($ini['summary']['main_language'] == $templateLanguage || $ini['summary']['main_language'] == 'International') {
+					$defaultParsers = array_unshift( $defaultParsers, substr($file,0,-4) );
 				}
-				else if (in_array($templateLanguage, createArray($ini['summary']['also_in'])))
-				{
+				else if (in_array($templateLanguage, createArray($ini['summary']['also_in']))) {
 					$defaultParsers[] = substr($file,0,-4);
 				}
 				unset($ini,$parserTitle);
@@ -85,8 +67,7 @@ class ISBN_parser extends InputCheck
 		return array($availableParsers,$defaultParsers);
 	}
 	
-	static public function prepareInput($input)
-	{
+	static public function prepareInput($input) {
 		return str_replace(array('-','x'),array('','X'),$input);
 	}
 }
