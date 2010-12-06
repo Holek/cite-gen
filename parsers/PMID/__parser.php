5c01d6666839466056343db7dfa66cfaa0b05649
Mike Połtyn <mike@poltyn.com>
2010-12-06 21:19:13 +0100

    reducing code size; easier to maintain

--- parsers/PMID/__parser.php
+++ parsers/PMID/__parser.php
@@ -1,32 +1,22 @@
 <?php
 
-
 class PMID_parser extends InputCheck
 {
 	static public function checkInput($input)
 	{
-		if (is_numeric($input) && $input > 0)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return (is_numeric($input) && $input > 0 && (int)$input == $input);
 	}
 
 	static public function listParsers()
 	{
-		$return = array(
-					'International' => array(
-											array(
-												'value' => 'PMID',
-												'label' => 'PubMED'
-											)
-										)
-					);
+		$return = array('International' => array(
+							array(
+								'value' => 'PMID',
+								'label' => 'PubMED'
+							)
+				));
 		return array($return,array('PMID'));
 	}
 }
 
-?>
+
