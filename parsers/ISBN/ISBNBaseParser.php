8f247d24bc3331ee3aca3ef7aa1f4b708b23c4a9
saper <saper@saper.info>
2012-03-27 14:11:42 +0000

    Refactor getOutput for ISBN parsers

--- parsers/ISBN/ISBNBaseParser.php
+++ parsers/ISBN/ISBNBaseParser.php
@@ -16,4 +16,42 @@ abstract class ISBNBaseParser extends Parser {
         protected $place;
         protected $source;
         protected $ISBN;
+
+	
+	/**
+	 * Book info getter
+	 *
+	 * @return     array
+	 */
+	public function getOutput()
+	{
+		// Array consisting of values, which keys are named
+		// after English Wikipedia {{Cite book}} template,
+		// with one exception: array '__names' consisting of
+		// subarrays 'last' and 'first'.
+		// These are equivalents of 'last' and 'first' fields,
+		// and these are arrays; some Wikipedias
+		// (ie. Polish) are using multiple author fields.
+		// In that case each last name in array has first name
+		// at the same index as it (ie. $this->lastNames[3]
+		// has to correspond with $this->firstNames[3];
+		// it has to be the same author)
+		
+		// Here you can also sort which fields are meant to be shown first
+		// at the generated template. Simply the first one goes first. ;)
+		return array(
+			'__names' => array(
+					'last' => $this->lastNames,
+					'first' => $this->firstNames
+				),
+			'title' => $this->title,
+			'date' => $this->date,
+			'publisher' => $this->publisher,
+			'location' => translateWithInterwiki($this->place, $this->getSiteLanguage()),
+			'isbn' => $this->ISBN,
+			'__sourceurl' => $this->source
+		);
+	}
+
+	abstract protected function getSiteLanguage();
 }
--- parsers/ISBN/isbnDB.php
+++ parsers/ISBN/isbnDB.php
@@ -10,6 +10,8 @@
 
 class isbnDB extends ISBNBaseParser {
 
+	protected $refname;
+
 	public function fetch($ISBN)
 	{
 		// To connect to ISBNdb.com database
@@ -74,40 +76,14 @@ class isbnDB extends ISBNBaseParser {
 		}
 	}
 
-	/**
-	 * Book info getter
-	 *
-	 * @return     array
-	 */
-	public function getOutput()
-	{
-		// Array consisting of values, which keys are named
-		// after English Wikipedia {{Cite book}} template,
-		// with one exception: array '__names' consisting of
-		// subarrays 'last' and 'first'.
-		// These are equivalents of 'last' and 'first' fields,
-		// and these are arrays; some Wikipedias
-		// (ie. Polish) are using multiple author fields.
-		// In that case each last name in array has first name
-		// at the same index as it (ie. $this->lastNames[3]
-		// has to correspond with $this->firstNames[3];
-		// it has to be the same author)
-		
-		// Here you can also sort which fields are meant to be shown first
-		// at the generated template. Simply the first one goes first. ;)
-		return array(
-			'__names' => array(
-					'last' => $this->lastNames,
-					'first' => $this->firstNames
-				),
-			'title' => $this->title,
-			'date' => $this->date,
-			'publisher' => $this->publisher,
-			'location' => translateWithInterwiki($this->place,'en'),
-			'isbn' => $this->ISBN,
-			'__sourceurl' => $this->source,
-			'__refname' => $this->refname
-		);
+	protected function getSiteLanguage() {
+		return 'en';
+	}
+
+	public function getOutput() {
+		$o = parent::getOutput();
+		$o['__refname'] = $this->refname;
+		return $o;
 	}
 }
 ?>
